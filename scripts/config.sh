c431844eaccd7f9541d5c3eff1a4bb256830bcd0
Jesús Almaraz Hernández <jesusalmarazhernandez@Jesuss-MacBook-Pro.local>
2021-07-02 02:17:58 +0200

    step 6 logic

--- scripts/config.sh
+++ scripts/config.sh
@@ -20,14 +20,29 @@ cd config
 
 # Get the needed URLs for the automation
 APIC_ADMIN_URL=`oc get routes -n ${APIC_NAMESPACE} | grep admin |  awk '{print $2}'`
-if [[ -z "${APIC_ADMIN_URL}" ]]; then echo "[ERROR][config.sh] - An error ocurred getting the IBM API Connect Admin URL"; exit 1; fi
+if [[ -z "${APIC_ADMIN_URL}" ]]; then echo "[ERROR][config.sh] - An error ocurred getting the IBM API Connect Admin url"; exit 1; fi
 APIC_API_MANAGER_URL=`oc get routes -n ${APIC_NAMESPACE} | grep api-manager |  awk '{print $2}'`
-if [[ -z "${APIC_API_MANAGER_URL}" ]]; then echo "[ERROR][config.sh] - An error ocurred getting the IBM API Connect Management URL"; exit 1; fi
+if [[ -z "${APIC_API_MANAGER_URL}" ]]; then echo "[ERROR][config.sh] - An error ocurred getting the IBM API Connect Management url"; exit 1; fi
+APIC_GATEWAY_URL=`oc get routes -n ${APIC_NAMESPACE} | grep gateway | grep -v manager | awk '{print $2}'`
+if [[ -z "${APIC_GATEWAY_URL}" ]]; then echo "[ERROR][config.sh] - An error ocurred getting the IBM API Connect Gateway url"; exit 1; fi
+APIC_GATEWAY_MANAGER_URL=`oc get routes -n ${APIC_NAMESPACE} | grep gateway-manager | awk '{print $2}'`
+if [[ -z "${APIC_GATEWAY_MANAGER_URL}" ]]; then echo "[ERROR][config.sh] - An error ocurred getting the IBM API Connect Gateway Manager url"; exit 1; fi
+APIC_ANALYTICS_CONSOLE_URL=`oc get routes -n ${APIC_NAMESPACE} | grep ac-endpoint | awk '{print $2}'`
+if [[ -z "${APIC_ANALYTICS_CONSOLE_URL}" ]]; then echo "[ERROR][config.sh] - An error ocurred getting the IBM API Connect Analytics Console url"; exit 1; fi
+APIC_PORTAL_DIRECTOR_URL=`oc get routes -n ${APIC_NAMESPACE} | grep portal-director | awk '{print $2}'`
+if [[ -z "${APIC_PORTAL_DIRECTOR_URL}" ]]; then echo "[ERROR][config.sh] - An error ocurred getting the IBM API Connect Portal Director url"; exit 1; fi
+APIC_PORTAL_WEB_URL=`oc get routes -n ${APIC_NAMESPACE} | grep portal-web | awk '{print $2}'`
+if [[ -z "${APIC_PORTAL_WEB_URL}" ]]; then echo "[ERROR][config.sh] - An error ocurred getting the IBM API Connect Portal Web url"; exit 1; fi
 
 # Storing the urls in the JSON config file
 echo "{" >> config.json
 echo "\"APIC_ADMIN_URL\":\"${APIC_ADMIN_URL}\"," >> config.json
 echo "\"APIC_API_MANAGER_URL\":\"${APIC_API_MANAGER_URL}\"," >> config.json
+echo "\"APIC_GATEWAY_URL\":\"${APIC_GATEWAY_URL}\"," >> config.json
+echo "\"APIC_GATEWAY_MANAGER_URL\":\"${APIC_GATEWAY_MANAGER_URL}\"," >> config.json
+echo "\"APIC_ANALYTICS_CONSOLE_URL\":\"${APIC_ANALYTICS_CONSOLE_URL}\"," >> config.json
+echo "\"APIC_PORTAL_DIRECTOR_URL\":\"${APIC_PORTAL_DIRECTOR_URL}\"," >> config.json
+echo "\"APIC_PORTAL_WEB_URL\":\"${APIC_PORTAL_WEB_URL}\"," >> config.json
 
 # Get the APIC CLI
 HTTP_CODE=`curl -s --write-out '%{http_code}' https://${APIC_ADMIN_URL}/client-downloads/toolkit-linux.tgz --insecure --output toolkit-linux.tgz`
--- scripts/config_apicv10.py
+++ scripts/config_apicv10.py
@@ -66,65 +66,157 @@ try:
     if DEBUG:
         print(info(3) + "Admin Org ID: " + admin_org_id)
 
-####################################
-# Step 4 - Create the Email Server #
-####################################
+# ####################################
+# # Step 4 - Create the Email Server #
+# ####################################
     
-    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/orgs/' + admin_org_id + '/mail-servers'
+#     url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/orgs/' + admin_org_id + '/mail-servers'
+    
+#     # Create the data object
+#     data = {}
+#     data['title'] = 'Default Email Server'
+#     data['name'] = 'default-email-server'
+#     data['host'] = os.environ['EMAIL_HOST']
+#     data['port'] = int(os.environ['EMAIL_PORT'])
+#     credentials = {}
+#     credentials['username'] = os.environ['EMAIL_USERNAME']
+#     credentials['password'] = os.environ['EMAIL_PASSWORD']
+#     data['credentials'] = credentials
+#     data['tls_client_profile_url'] = None
+#     data['secure'] = False
+
+#     if DEBUG:
+#         print(info(4) + "This is the data object:")
+#         print(info(4), data)
+#         print(info(4) + "This is the JSON dump:")
+#         print(info(4), json.dumps(data))
+
+#     response = api_calls.make_api_call(url, admin_bearer_token, 'post', data)
+
+#     if response.status_code != 201:
+#           raise Exception("Return code for creating the Email Server isn't 201. It is " + str(response.status_code))
+#     email_server_url = response.json()['url']
+#     if DEBUG:
+#         print(info(4) + "Email Server url: " + email_server_url)
+
+# ##################################################
+# # Step 5 - Sender and Email Server Configuration #
+# ##################################################
+
+#     url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/cloud/settings'
+    
+#     # Create the data object
+#     # Ideally this would also be loaded from a sealed secret
+#     data = {}
+#     data['mail_server_url'] = email_server_url
+#     email_sender = {}
+#     email_sender['name'] = 'APIC Administrator'
+#     email_sender['address'] = 'test@test.com'
+#     data['email_sender'] = email_sender
+
+#     if DEBUG:
+#         print(info(5) + "This is the data object:")
+#         print(info(5), data)
+#         print(info(5) + "This is the JSON dump:")
+#         print(info(5), json.dumps(data))
+
+#     response = api_calls.make_api_call(url, admin_bearer_token, 'put', data)
+
+#     if response.status_code != 200:
+#           raise Exception("Return code for Sender and Email Server configuration isn't 200. It is " + str(response.status_code))
+
+#######################################
+# Step 6 - Register a Gateway Service #
+#######################################
+
+    # First, we need to get the Datapower API Gateway instances details
+
+    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/cloud/integrations/gateway-service/datapower-api-gateway'
+
+    response = api_calls.make_api_call(url, admin_bearer_token, 'get')
+
+    if response.status_code != 200:
+          raise Exception("Return code for getting the Datapower API Gateway instances details isn't 200. It is " + str(response.status_code))
+
+    datapower_api_gateway_url = response.json()['url']
+    if DEBUG:
+        print(info(6) + "Email Server url: " + datapower_api_gateway_url)
+
+    # Second, we need to get the TLS server profiles
+
+    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/orgs/' + admin_org_id + '/tls-server-profiles'
+
+    response = api_calls.make_api_call(url, admin_bearer_token, 'get')
+
+    found = False
+    tls_server_profile_url = ''
+    if response.status_code != 200:
+          raise Exception("Return code for getting the TLS server profiles isn't 200. It is " + str(response.status_code))
+    for profile in response.json()['results']:
+        if profile['name'] == "tls-server-profile-default":
+            found = True
+            tls_server_profile_url = profile['url']
+    if not found:
+        raise Exception("[ERROR] - The default TLS server profile was not found in the IBM API Connect Cluster instance")
+
+    if DEBUG:
+        print(info(6) + "Default TLS server profile url: " + tls_server_profile_url)
+
+    # Third, we need to get the TLS client profiles
+
+    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/orgs/' + admin_org_id + '/tls-client-profiles'
+
+    response = api_calls.make_api_call(url, admin_bearer_token, 'get')
+
+    found = False
+    tls_client_profile_url = ''
+    if response.status_code != 200:
+          raise Exception("Return code for getting the TLS client profiles isn't 200. It is " + str(response.status_code))
+    for profile in response.json()['results']:
+        if profile['name'] == "gateway-management-client-default":
+            found = True
+            tls_client_profile_url = profile['url']
+    if not found:
+        raise Exception("[ERROR] - The Gateway Management TLS client profile was not found in the IBM API Connect Cluster instance")
+
+    if DEBUG:
+        print(info(6) + "Gateway Management TLS server profile url: " + tls_client_profile_url)
+
+
+    # Finally, we can actually make the REST call to get the Default Gateway Service registered
+
+    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/orgs/' + admin_org_id + '/availability-zones/availability-zone-default/gateway-services'
     
     # Create the data object
     data = {}
-    data['title'] = 'Default Email Server'
-    data['name'] = 'default-email-server'
-    data['host'] = os.environ['EMAIL_HOST']
-    data['port'] = int(os.environ['EMAIL_PORT'])
-    credentials = {}
-    credentials['username'] = os.environ['EMAIL_USERNAME']
-    credentials['password'] = os.environ['EMAIL_PASSWORD']
-    data['credentials'] = credentials
-    data['tls_client_profile_url'] = None
-    data['secure'] = False
+    data['name'] = "default-gateway-service"
+    data['title'] = "Default Gateway Service"
+    data['summary'] = "Default Gateway Service that comes out of the box with API Connect Cluster v10"
+    data['endpoint'] = 'https://' + environment_config["APIC_GATEWAY_MANAGER_URL"]
+    data['api_endpoint_base'] = 'https://' + environment_config["APIC_GATEWAY_URL"]
+    data['tls_client_profile_url'] = tls_client_profile_url
+    data['gateway_service_type'] = 'datapower-api-gateway'
+    visibility = {}
+    visibility['type'] = 'public'
+    data['visibility'] = visibility
+    sni = []
+    sni_inner={}
+    sni_inner['host'] = '*'
+    sni_inner['tls_server_profile_url'] = tls_server_profile_url
+    sni.append(sni_inner)
+    data['sni'] = sni
+    data['integration_url'] = datapower_api_gateway_url
 
     if DEBUG:
-        print(info(4) + "This is the data object:")
-        print(info(4), data)
-        print(info(4) + "This is the JSON dump:")
-        print(info(4), json.dumps(data))
+        print(info(6) + "This is the data object:")
+        print(info(6), data)
+        print(info(6) + "This is the JSON dump:")
+        print(info(6), json.dumps(data))
 
     response = api_calls.make_api_call(url, admin_bearer_token, 'post', data)
 
-    email_server_url = ''
     if response.status_code != 201:
-          raise Exception("Return code for creating the Email Server isn't 201. It is " + str(response.status_code))
-    email_server_url = response.json()['url']
-    if DEBUG:
-        print(info(4) + "Email Server url: " + email_server_url)
-
-##################################################
-# Step 5 - Sender and Email Server Configuration #
-##################################################
-
-    url = 'https://' + environment_config["APIC_ADMIN_URL"] + '/api/cloud/settings'
-    
-    # Create the data object
-    # Ideally this would also be loaded from a sealed secret
-    data = {}
-    data['mail_server_url'] = email_server_url
-    email_sender = {}
-    email_sender['name'] = 'APIC Administrator'
-    email_sender['address'] = 'test@test.com'
-    data['email_sender'] = email_sender
-
-    if DEBUG:
-        print(info(5) + "This is the data object:")
-        print(info(5), data)
-        print(info(5) + "This is the JSON dump:")
-        print(info(5), json.dumps(data))
-
-    response = api_calls.make_api_call(url, admin_bearer_token, 'put', data)
-
-    if response.status_code != 200:
-          raise Exception("Return code for Sender and Email Server configuration isn't 200. It is " + str(response.status_code))
+          raise Exception("Return code for registering the Default Gateway Service isn't 201. It is " + str(response.status_code))
 
 except Exception as e:
     raise Exception("[ERROR] - Exception in " + FILE_NAME + ": " + repr(e))
